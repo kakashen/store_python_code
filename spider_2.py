eb6496cb4225a67ed622ba4c3853cb5c95f981ce
kakashen <1519176958@qq.com>
2019-04-25 17:58:14 +0800

    获取html数据

--- spider_2.py
+++ spider_2.py
@@ -1,18 +1,49 @@
+import time
 import requests
 from bs4 import BeautifulSoup
 
 
-def spider():
+def get_urls():
+    base_url = 'https://rizhao.58.com/ershoufang/0/'
+
+    response = requests.get(base_url)
+    with open("base.html", "w", encoding="utf-8") as f:
+        f.write(response.text)
+
+    # f = open('base.html', 'r', encoding='utf8')
+    # string = f.read()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # print(soup.select('.house-list-wrap')[0].li['logr'])
+
+    li = soup.select('.house-list-wrap')[0].find_all('li')
+
+    for i in li:
+        total = connect(i.select('.sum')[0].strings)  # 总价
+        href = i.find('a')['href']  # url 地址
+        # print(href)
+        num = i['logr'].find('@postdate:') + 10  # 时间 ms
+        # print(i['logr'][num:num+13])
+
+        data = {'link': href, 'sum': total, 'post_date': num//1000}
+        ret = requests.post('http://www.xitou.online/api/python', data)
+
+        if ret:
+            spider(href)
+            print(href)
+        time.sleep(1)
+
+
+def spider(url):
     # url = 'https://rizhao.58.com/ershoufang/37886495714975x.shtml'
-    # response = requests.get(url)
-    # # print(response.text)
-    # with open("1.html", "w", encoding="utf-8") as f:
-    #     f.write(response.text)
+    response = requests.get(url)
+    # print(response.text)
+    with open("1.html", "w", encoding="utf-8") as f:
+        f.write(response.text)
 
-    f = open('1.html', 'r', encoding='utf8')
-    string = f.read()
+    # f = open('1.html', 'r', encoding='utf8')
+    # string = f.read()
 
-    soup = BeautifulSoup(string, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
     tel_tag = soup.select('.phone-num')
 
@@ -26,6 +57,13 @@ def spider():
     area = trim(main[0].string)  # 面积
 
     region = trim(connect(house_basic_item3[1].strings))  # 地区
+
+    # db(tel, community, area, region)
+
+    data = {'link': url, 'phone': tel, 'community': community, 'area': area, 'region': region}
+    ret = requests.post('http://www.xitou.online/api/python', data)
+    if ret:
+        print(data)
 
 
 def connect(string):
@@ -50,4 +88,4 @@ def trim(string):
 
 
 if __name__ == '__main__':
-    spider()
+    get_urls()
